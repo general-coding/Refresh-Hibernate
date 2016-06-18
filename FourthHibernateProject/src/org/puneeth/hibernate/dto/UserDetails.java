93af17bc84068431c505d9dc15c03e60259f94cd
Puneeth U Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2016-06-17 22:54:24 -0500

    @Temporal(TemporalType.DATE) will only choose date. Not the timestamp

--- FourthHibernateProject/src/org/puneeth/hibernate/dto/UserDetails.java
+++ FourthHibernateProject/src/org/puneeth/hibernate/dto/UserDetails.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
 @Entity
@@ -14,9 +16,26 @@ public class UserDetails {
 	private int userId;
 	@Transient
 	private String userName;
+	@Temporal(TemporalType.DATE)
 	private Date joinedDate;
 	private String Address;
 	private String description;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
 	public Date getJoinedDate() {
 		return joinedDate;
@@ -40,22 +59,6 @@ public class UserDetails {
 
 	public void setDescription(String description) {
 		this.description = description;
-	}
-
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	public String getUserName() {
-		return userName + " from getter";
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
 	}
 
 }
