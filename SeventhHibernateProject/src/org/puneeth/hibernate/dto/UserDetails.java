808ec7873022c171a2596376fa9412cce28e2c18
Puneeth U Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2016-06-18 00:26:43 -0500

    Get data from DB

---
+++ SeventhHibernateProject/src/org/puneeth/hibernate/dto/UserDetails.java
@@ -0,0 +1,65 @@
+package org.puneeth.hibernate.dto;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "USER_DETAILS")
+public class UserDetails {
+	@Id
+	private int userId;
+	private String userName;
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+	private String Address;
+	@Lob
+	private String description;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ SixthHibernateProject/src/org/puneeth/hibernate/HibernateTest.java
@@ -0,0 +1,38 @@
+package org.puneeth.hibernate;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.puneeth.hibernate.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		UserDetails user = new UserDetails();
+		user.setUserId(1);
+		user.setUserName("First Person");
+		user.setAddress("First User's Address");
+		user.setJoinedDate(new Date());
+		user.setDescription("Description of the user goes here");
+
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+
+		user = null;
+
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		user = session.get(UserDetails.class, 1);
+		System.out.println("User Name retrieved: " + user.getUserName());
+		
+		System.gc();
+		System.exit(0);
+	}
+
+}
