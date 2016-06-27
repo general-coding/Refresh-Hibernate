a9a99e28914ce28d2595fa9dfd2596fb39ce5cb1
Puneeth U Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2016-06-27 16:58:52 -0500

    Start CRUD with clean slate.

--- TwentyFirstHibernateProject/src/org/puneeth/hibernate/HibernateTest.java
+++ TwentyFirstHibernateProject/src/org/puneeth/hibernate/HibernateTest.java
@@ -3,30 +3,15 @@ package org.puneeth.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.puneeth.hibernate.dto.FourWheeler;
-import org.puneeth.hibernate.dto.TwoWheeler;
-import org.puneeth.hibernate.dto.Vehicle;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		Vehicle vehicle = new Vehicle();
-		vehicle.setVehicleName("Car");
-
-		TwoWheeler bike = new TwoWheeler();
-		bike.setVehicleName("Bike");
-		bike.setSteeringHandle("Bike Steering Handle");
-
-		FourWheeler car = new FourWheeler();
-		car.setVehicleName("Car");
-		car.setSteeringWheel("Car Steering Wheel");
-
+		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		session.save(vehicle);
-		session.save(bike);
-		session.save(car);
+		
 		session.getTransaction().commit();
 		session.close();
 		System.exit(0);
