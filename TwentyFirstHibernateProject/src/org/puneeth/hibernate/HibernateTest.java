0d1e0e3bc4955c98d5ea80b337d0b0042e437945
Puneeth U Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2016-06-27 17:05:56 -0500

    Created 10 objects and saved it to DB.

--- TwentyFirstHibernateProject/src/org/puneeth/hibernate/HibernateTest.java
+++ TwentyFirstHibernateProject/src/org/puneeth/hibernate/HibernateTest.java
@@ -3,15 +3,22 @@ package org.puneeth.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.puneeth.hibernate.dto.UserDetails;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		
+
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		
+
+		for (int i = 0; i < 10; i++) {
+			UserDetails user = new UserDetails();
+			user.setUserName("User " + i);
+			session.save(user);
+		}
+
 		session.getTransaction().commit();
 		session.close();
 		System.exit(0);
--- TwentyFirstHibernateProject/src/org/puneeth/hibernate/dto/UserDetails.java
+++ TwentyFirstHibernateProject/src/org/puneeth/hibernate/dto/UserDetails.java
@@ -1,18 +1,11 @@
 package org.puneeth.hibernate.dto;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "USER_DETAILS")
 public class UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
